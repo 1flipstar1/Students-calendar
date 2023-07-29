e24e575bdf9054270429cd57e0dd9565409e5058
flipstar1 <flip014@mail.ru>
2023-07-29 15:00:32 +0300

    com

--- webdriver.py
+++ webdriver.py
@@ -1,24 +1,34 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 import json
 
 opt = webdriver.ChromeOptions()
-opt.add_argument('user-agent=Mozila/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0')
+opt.add_argument(
+    'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36')
 driver = webdriver.Chrome(executable_path='chromedriver.exe', options=opt)
 driver.get('https://school.admoblkaluga.ru/esia')
 time.sleep(5)
 driver.find_element(By.CLASS_NAME, 'style_intro-form_action__btn__2M9jV').click()
 time.sleep(5)
 driver.find_element(By.ID, 'login').send_keys('+79533299805')
+pas = driver.find_element(By.ID, 'password')
+pas.send_keys('Bessudnov!08')
 time.sleep(5)
-driver.find_element(By.ID, 'password').send_keys('Bessudnov!08')
-time.sleep(5)
-driver.find_element(By.CLASS_NAME, 'plain-button plain-button_wide').click()
+pas.send_keys(Keys.ENTER)
+auth_key = input()
+key_sel = []
 
+driver.find_element(By.CLASS_NAME, 'code-input text-left').send_keys(auth_key)
+# driver.find_element(By.LINK_TEXT, 'Позже').click()
+# try:
+#     driver.find_element(By.CLASS_NAME, 'plain-button-inline').click()
+# except:
+#     print(54)
 input()
 
 #
