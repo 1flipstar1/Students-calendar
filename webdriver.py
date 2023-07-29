f64b0af686eb1bee5a165dba7bc9f12e429f10cd
flipstar <flip014@mail.ru>
2023-07-29 22:24:39 +0300

    beta

--- webdriver.py
+++ webdriver.py
@@ -1,5 +1,5 @@
 import time
-
+from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,28 +8,37 @@ from selenium.webdriver.support.wait import WebDriverWait
 import json
 
 opt = webdriver.ChromeOptions()
-opt.add_argument(
-    'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36')
-driver = webdriver.Chrome(executable_path='chromedriver.exe', options=opt)
+service = Service(executable_path='./chromedriver.exe')
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=options)
 driver.get('https://school.admoblkaluga.ru/esia')
-time.sleep(5)
+time.sleep(2)
+driver.find_element(By.ID, 'details-button').click()
+driver.find_element(By.ID, 'proceed-link').click()
+time.sleep(2)
+
 driver.find_element(By.CLASS_NAME, 'style_intro-form_action__btn__2M9jV').click()
-time.sleep(5)
+time.sleep(2)
 driver.find_element(By.ID, 'login').send_keys('+79533299805')
 pas = driver.find_element(By.ID, 'password')
 pas.send_keys('Bessudnov!08')
-time.sleep(5)
+time.sleep(2)
 pas.send_keys(Keys.ENTER)
-auth_key = input()
+for request in driver.requests:
+    if request.method == "type_of_method_you_want" and request.url == "request_url_you_want":
+        if request.headers.get("name_of_token_you_want"): #to confirm it exists before assigning a variable to it.
+            token = request.headers.get("name_of_token_you_want")
+            print(token)
+""" auth_key = input()
 key_sel = []
 
-driver.find_element(By.CLASS_NAME, 'code-input text-left').send_keys(auth_key)
+driver.find_element(By.CLASS_NAME, 'code-input text-left').send_keys(auth_key) """
 # driver.find_element(By.LINK_TEXT, 'Позже').click()
 # try:
 #     driver.find_element(By.CLASS_NAME, 'plain-button-inline').click()
 # except:
 #     print(54)
-input()
+
 
 #
 # wait = WebDriverWait(driver, 10)
